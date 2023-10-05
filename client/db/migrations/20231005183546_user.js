85ffb238376cbcf4a5bad25ee1703d8d99b68e90
iven-yao <chihkeny@usc.edu>
2023-10-05 11:41:13 -0700

    integrate postgres database with docker compose and knex.js

---
+++ client/db/migrations/20231005183546_user.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("users", table => {
+    table.increments().primary();
+    table.string("username", 128).notNullable();
+    table.string("profile_img", 128).notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("users");
+};
